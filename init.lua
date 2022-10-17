8929ff84ac2112cdf201b7925a8c521f7cfbb34a
Maurer Peter <97maurerpeter@gmail.com>
2022-10-17 20:36:19 +0300

    Keymaps: yank/delete wtih(out) system clipboard and auto-centering during movement

--- init.lua
+++ init.lua
@@ -1,9 +1,5 @@
 --              AstroNvim Configuration Table
--- All configuration changes should go inside of the table below
 
--- You can think of a Lua "table" as a dictionary like data structure the
--- normal format is "key = value". These also handle array like data structures
--- where a value with no key simply has an implicit numeric key
 local config = {
   colorscheme = "onedark",
 
@@ -23,17 +19,32 @@ local config = {
     n = {
       ["<leader>r"] = { ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
         desc = "Replace selection occurrences with sed" },
+      ["<C-d>"] = { "<C-d>zz", desc = "Scroll down half a screen and center" },
+      ["<C-u>"] = { "<C-u>zz", desc = "Scroll up half a screen and center" },
+      ["<leader>y"] = { "\"+y", desc = "Yank to system clipboard" },
+      ["<leader>D"] = { "<cmd>Alpha<cr>", desc = "Alpha Dashboard" },
+      ["<leader>d"] = { "\"_d", desc = "Delete without yanking to register" },
+      ["n"] = { "nzzzv", desc = "Go to next match and center" },
+      ["N"] = { "Nzzzv", desc = "Go to previous match and center" },
     },
 
     -- visual mode
     v = {
       ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move selection down by a line" },
       ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move selection up by a line" },
+      ["<leader>y"] = { "\"+y", desc = "Yank to system clipboard" },
+      ["<leader>d"] = { "\"_d", desc = "Delete without yanking to register" },
     },
 
     -- insert mode
     i = {
       ["<C-c>"] = { "<Esc>", desc = "Exit insert mode via Ctrl+c" },
+    },
+  },
+
+  options = {
+    opt = {
+      spell = true, -- sets vim.opt.spell
     },
   },
 }
